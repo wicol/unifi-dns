09d18c4475af75c65442759f35dea7da39ee8a48
wicol <wicol@users.noreply.github.com>
2019-03-30 22:09:38 +0100

    Use diff -N to allow for non existing files

--- run.sh
+++ run.sh
@@ -3,12 +3,11 @@ hosts_dir=/etc/dnsmasq.hosts
 unifi_hosts=$hosts_dir/unifi.hosts
 
 [ -d $hosts_dir ] || mkdir $hosts_dir
-touch $unifi_hosts
 dnsmasq --keep-in-foreground --hostsdir=$hosts_dir --log-facility=- ${DNSMASQ_OPTS} &
 
 while true; do
     ./get_unifi_reservations.py > /tmp/fetched_unifi.hosts
-    if ! diff $unifi_hosts /tmp/fetched_unifi.hosts; then
+    if ! diff -N $unifi_hosts /tmp/fetched_unifi.hosts; then
         mv /tmp/fetched_unifi.hosts $unifi_hosts
     fi
     sleep ${UNIFI_POLL_INTERVAL:-60}
